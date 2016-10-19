535d4c1f25431de70491879092e6359dd1576f41
Kevin Waters <wnivek@gmail.com>
2016-10-18 21:18:24 -0400

    Added ship movement.
    Also realized Hero ship had been made a child of the camera. Corrected
    this.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class Hero : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    static public Hero S;
+
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    public float shieldLevel = 1;
+
+    public bool ____________________________;
+
+	void Awake () {
+        S = this;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 	}
 }
