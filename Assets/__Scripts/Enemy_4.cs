c2e27d651799a64e31b3f61424ba3b738d0b3f07
Kevin Waters <wnivek@gmail.com>
2016-10-20 12:09:18 -0400

    Enemy 4 movement scripted

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy_4 : MonoBehaviour {
+public class Enemy_4 : Enemy {
 
-	// Use this for initialization
+    public Vector3[] points;
+    public float timeStart;
+    public float duration = 4;
+
 	void Start () {
-	
+        points = new Vector3[2];
+        points[0] = pos;
+        points[1] = pos;
+
+        InitMovement();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void InitMovement() {
+        Vector3 p1 = Vector3.zero;
+        float esp = Main.S.enemySpawnPadding;
+        Bounds cBounds = Utils.camBounds;
+        p1.x = Random.Range(cBounds.min.x + esp, cBounds.max.x - esp);
+        p1.y = Random.Range(cBounds.min.y + esp, cBounds.max.y - esp);
+
+        points[0] = points[1];
+        points[1] = p1;
+
+        timeStart = Time.time;
+    }
+
+    public override void Move() {
+        float u = (Time.time - timeStart) / duration;
+        if (u >= 1) {
+            InitMovement();
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow(1 - u, 2);
+
+        pos = (1 - u) * points[0] + u * points[1];
+    }
 }
